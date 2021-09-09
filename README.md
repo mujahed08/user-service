6a7fb69eaa39884d04a9e98afb552257846207ff
Mohammed Mujahed Khan <mujahed08@hotmail.com>
2021-09-09 20:14:30 +0530

    API expose

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ pylint --generate-rcfile > .pylintrc
 pytest.ini is a configuration file for pytest having higest configuration precedence. -s argument says python print() function 
 prints to the console.
 
+To generate pydantic model for API
+
+user-service>datamodel-codegen --url http://localhost:7001/openapi.json --output USER_SERVICE\pydantic_models\generated\user_svc_pcs\models.py
+
 
 To Start applicationuse the following command
-user-service-system>uvicorn USER_SERVICE.main:app --port 8002 --log-config log4py.yaml --reload --reload-dir USER_SERVICE
+user-service>uvicorn USER_SERVICE.main:app --port 8002 --log-config log4py.yaml --reload --reload-dir USER_SERVICE
--- USER_SERVICE/main.py
+++ USER_SERVICE/main.py
@@ -1,27 +1,58 @@
 """
 FastAPI
 """
+import httpx
 
-from USER_SERVICE.pydantic.holders import Example, Status
+from fastapi import FastAPI
+from USER_SERVICE.commons.logger import get_logger
+from USER_SERVICE.pydantic_models.models import Response
+from USER_SERVICE.pydantic_models.generated.user_svc_pcs.models import UserIn
+from USER_SERVICE.pydantic_models.enviornment import get_settings
 
 logger = get_logger('main.py')
-logger.info(Example.schema_json())
+
+settings = get_settings()
+
+url_signup = f'{settings.user_svc_pcs_host}/signup'
+
+def get_url_activate(user_id:int):
+    return f'{settings.user_svc_pcs_host}/activate/{user_id}'
+
+def get_url_deactivate(user_id:int):
+    return f'{settings.user_svc_pcs_host}/deactivate/{user_id}'
 
 logger.info('   Initiliazing Fast API app')
 app = FastAPI(title="FastAPI")
-logger.info('   Initialized Fast API app')
 
-@app.get("/example", response_model=Status)
-async def get_example():
-    logger.info('   get_example function is executing')
-	message = 'Hello World!'
-    return Status(message=f'Returning with message: {message}')
+@app.post("/signup", response_model=Response)
+async def signup(user:UserIn):
+    logger.info('   signup function is executing')
+    logger.info(user.json())
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url_signup, data=user.json())
+        if response.status_code == 200:
+            data1 = response.json()
+            logger.info(data1)
+            return Response(code=200, message='SUCCESS', data=data1)
 
-@app.post("/example/ops", response_model=Example,
-responses={404: {"model": HTTPNotFoundError}})
-async def example_ops(example:Example):
-    logger.info('   example_ops function is executing')
-    logger.info(example.json())
-    return Example(id=1, name='Hellow World!')
+@app.get("/deactivate/{user_id}", response_model=Response)
+async def deactivate(user_id:int):
+    logger.info('   deactivate function is executing')
+    return await toggle_enabled(user_id, False)
+
+@app.get("/activate/{user_id}", response_model=Response)
+async def activate(user_id:int):
+    logger.info('   activate function is executing')
+    return await toggle_enabled(user_id, True)
+
+async def toggle_enabled(user_id:int, enabled: bool):
+    logger.info('   toggle_enabled function is executing')
+    async with httpx.AsyncClient() as client:
+        url_toggle = get_url_activate(user_id) if enabled else get_url_deactivate(user_id)
+        response = await client.get(url_toggle)
+        data1 = response.json()
+        logger.info(data1)
+        if response.status_code == 200:
+            return Response(code=200, message='SUCCESS', data=data1)
 
 logger.info('   End of the main file')
